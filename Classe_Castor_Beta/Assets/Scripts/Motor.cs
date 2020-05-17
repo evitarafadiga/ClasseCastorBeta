50f4804c8470a69beb0282f2c33416e35a2d99ed
Lázaro <lazaro@pop-os.localdomain>
2020-05-17 03:46:17 -0300

    Severas alterações in-game.

---
+++ Classe_Castor_Beta/Assets/Scripts/Motor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+
+    public float moveSpeed = 15.0f;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+    private Vector3 direction;
+
+    private void FixedUpdate()
+    {
+        direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        rb.AddForce(direction * moveSpeed);
+    }
+}
