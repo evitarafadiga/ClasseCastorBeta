1bef481d8ccd317922af9110db7bc8c24f85f58a
Lázaro Santos <47966985+evitarafadiga@users.noreply.github.com>
2020-05-24 02:26:32 -0300

    Teste de cenário
    Adicionado junto com outras melhorias.

---
+++ Classe_Castor_Beta/Assets/Scripts/UserInfoManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserInfoManager : MonoBehaviour
+{
+    public GameObject UserInfoMenu;
+
+    private void Start()
+    {
+        UserInfoMenu.SetActive (false);
+    }
+    
+    public void TogglehideUserInfoMenu()
+    {
+        UserInfoMenu.SetActive (!UserInfoMenu.activeSelf);
+    }
+   
+}
